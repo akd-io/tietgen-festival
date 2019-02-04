c3d9d5712f583e2d51016aafccf859591b30a33d
Anders Kjær Damgaard <akd@akd.io>
2019-02-04 21:51:05 +0100

    Add Modal placeholder and make Index stateful, holding modal props in state.

---
+++ components/Modal.jsx
@@ -0,0 +1,4 @@
+import { Fragment } from "react";
+export default () => {
+  <Fragment />;
+};
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,49 @@
-import { Fragment } from "react";
+import { Fragment, Component } from "react";
 import { BannerSection } from "../components/sections/BannerSection";
 import { ArtistsSection } from "../components/sections/ArtistsSection/ArtistsSection";
 import { ActivitiesSection } from "../components/sections/ActivitiesSection/ActivitiesSection";
 import { AboutSection } from "../components/sections/AboutSection";
+import { Modal } from "../components/Modal";
 import Menu from "../components/Menu";
-export default () => (
-  <Fragment>
-    <Menu />
-    <BannerSection />
-    <ArtistsSection />
-    <ActivitiesSection />
-    <AboutSection />
-    {/* TODO: Add DownloadSection */}
-    <style jsx global>{`
-      /* TypoPRO Bebas Neue Regular */
-      @import url("https://cdn.jsdelivr.net/npm/@typopro/web-bebas-neue@3.7.5/TypoPRO-BebasNeue-Regular.css");
-      html,
-      body {
-        margin: 0;
-      }
-      body {
-        font-family: sans-serif;
-      }
-      h1,
-      h2,
-      h3,
-      h4,
-      h5,
-      h6 {
-        font-family: "TypoPRO Bebas Neue", sans-serif;
-      }
-    `}</style>
-  </Fragment>
-);
+
+export default class Index extends Component {
+  state = {
+    modalProps: null
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Menu />
+
+        <BannerSection />
+        <ArtistsSection />
+        <ActivitiesSection />
+        <AboutSection />
+        {/* TODO: Add DownloadSection */}
+
+        {this.state.modal ? <Modal {...this.state.modalProps} /> : null}
+
+        <style jsx global>{`
+          /* TypoPRO Bebas Neue Regular */
+          @import url("https://cdn.jsdelivr.net/npm/@typopro/web-bebas-neue@3.7.5/TypoPRO-BebasNeue-Regular.css");
+          html,
+          body {
+            margin: 0;
+          }
+          body {
+            font-family: sans-serif;
+          }
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            font-family: "TypoPRO Bebas Neue", sans-serif;
+          }
+        `}</style>
+      </Fragment>
+    );
+  }
+}
