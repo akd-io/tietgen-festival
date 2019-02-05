c81b832b754381031dcfa5441477a78083767b08
Anders Kjær Damgaard <akd@akd.io>
2019-02-05 19:17:19 +0100

    Add video and logo to banner section

--- components/sections/BannerSection.js
+++ components/sections/BannerSection.js
@@ -6,21 +6,47 @@ export const BannerSection = () => (
   <Fragment>
     <Section
       id="banner-section"
-      backgroundImage={"/static/image.png"}
       height="calc(100vh - 80px)"
       color={colors.yellow}
     >
-      <h1 className="bannerTitle">TIETGEN FESTIVAL</h1>
-      <h2 className="bannerSubTitle">May 5th 2019</h2>
+      <video autoPlay loop muted>
+        <source src="/static/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="container">
+        <img src="static/logo.png" />
+        <div className="subtitles">
+          <h2 className="bannerSubTitle">May 4-5 2019</h2>
+          <h2 className="bannerSubTitle">Tietgenkollegiet</h2>
+        </div>
+      </div>
     </Section>
     <style jsx>{`
-      .bannerTitle {
-        margin: 0;
-        font-size: calc(25px + 5vw);
+      video {
+        position: absolute;
+        width: 100%;
+        height: calc(100vh - 80px);
+        z-index: -1;
+        object-fit: cover;
+      }
+      .container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+      img {
+        width: 25em;
+        margin-right: 2.5em;
+        border-radius: 100%;
+      }
+      .subtitles {
+        display: flex;
+        flex-direction: column;
       }
       .bannerSubTitle {
         margin: 0;
-        font-size: calc(20px + 4vw);
+        font-size: calc(25px + 3vw);
+        text-shadow: 0px 0px 50px rgba(0, 0, 0, 0.5);
       }
     `}</style>
   </Fragment>
