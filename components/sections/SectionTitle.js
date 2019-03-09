bdb176fd8feb08569f1a90c511c008d889eab73a
Anders Kjær Damgaard <akd@akd.io>
2019-03-09 12:43:00 +0100

    Enlarge titles

--- components/sections/SectionTitle.js
+++ components/sections/SectionTitle.js
@@ -3,7 +3,7 @@ export const SectionTitle = props => (
     {props.children}
     <style jsx>{`
       h1 {
-        font-size: calc(2em + 1vw);
+        font-size: calc(5em + 1vw);
         text-transform: uppercase;
       }
     `}</style>
