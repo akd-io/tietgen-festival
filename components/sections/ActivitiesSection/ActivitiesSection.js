3b7362d4cf33b01d82f3b07a714076cf3e0c62bd
Anders Kjær Damgaard <akd@akd.io>
2019-03-20 14:40:48 +0100

    Change Activities section to TBA

--- components/sections/ActivitiesSection/ActivitiesSection.js
+++ components/sections/ActivitiesSection/ActivitiesSection.js
@@ -1,53 +1,25 @@
-import { Section } from "../Section";
-import { Activity } from "./Activity";
-import { SectionTitle } from "../SectionTitle";
-import colors from "../../colors";
+import { Fragment } from 'react';
+import { Section } from '../Section';
+import { Activity } from './Activity';
+import { SectionTitle } from '../SectionTitle';
+import colors from '../../colors';
 
 export const ActivitiesSection = () => {
-  const activities = [
-    {
-      name: "Blind Game of Tag",
-      startTime: "13:00",
-      endTime: "14:00"
-    },
-    {
-      name: "Werewolf",
-      startTime: "13:00",
-      endTime: "14:00"
-    },
-    {
-      name: "Blind Cooking Challenge",
-      startTime: "13:00",
-      endTime: "14:00"
-    },
-    {
-      name: "Silent Disco",
-      startTime: "13:00",
-      endTime: "14:00"
-    },
-    {
-      name: "Activity 5",
-      startTime: "13:00",
-      endTime: "14:00"
-    },
-    {
-      name: "Activity 6",
-      startTime: "13:00",
-      endTime: "14:00"
-    },
-    {
-      name: "Activity 7",
-      startTime: "13:00",
-      endTime: "14:00"
-    }
-  ];
-
   return (
-    <Section id="activities-section" color={colors.pink} backgroundColor={colors.lightblue}>
-      <SectionTitle>ACTIVITIES</SectionTitle>
-      {activities.map((activity, index) => (
-        <Activity activity={activity} key={activity.name} />
-      ))}
-    </Section>
+    <Fragment>
+      <Section
+        id="activities-section"
+        color={colors.pink}
+        backgroundColor={colors.lightblue}
+      >
+        <SectionTitle>ACTIVITIES</SectionTitle>
+        <h2>TO BE ANNOUNCED!</h2>
+      </Section>
+      <style jsx>{`
+        h2 {
+          font-size: calc(1.4em + 1.4vw);
+        }
+      `}</style>
+    </Fragment>
   );
 };
